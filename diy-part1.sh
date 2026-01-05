1dbdb97b194a8b799580bc0a5a083aa9fb77ce30
XIAOZHAOXSXH <112842045+XIAOZHAOXSXH@users.noreply.github.com>
2026-01-05 10:04:37 +0800

    Update feeds.conf.default with new sources
    Added iStoreOS and Kenzo sources to feeds configuration.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# diy-part1.sh
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加 iStoreOS 核心源 (首页、商店、依赖)
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 添加 Kenzo 源 (OpenClash, MosDNS, Modemband)
+# Kenzo 是目前最全的源，用来替代失效的 Sirpdboy
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
